fe0851d81caf70c7884f4e528490524313414463
KillianMau <maudet.killian.loq@gmail.com>
2025-05-27 11:53:22 +0200

    add message for processing

--- R/main_fragmentomics.R
+++ R/main_fragmentomics.R
@@ -180,6 +180,9 @@ process_fragmentomics <- function(
     ref_final <- mut_info_final[i, 3]
     alt_final <- mut_info_final[i, 4]
 
+    # Message
+    cat(paste0("Processing ", chr_final, ":", pos_final, " = ", ref_final, ">", alt_final, "\n"))
+
     # Return the mutation status in SNV, ins, del, MNP
     mutation_type <- define_mutation_status(ref_final, alt_final)
 
